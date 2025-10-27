53ee1add182ce633deff2d1a18a8c01b1169ea7a
Renaud Montes <rmontesl@hotmail.com>
2025-10-27 09:31:22 -0700

    Initial Commit

---
+++ Pee Tracker Watch App/Pee_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pee_TrackerApp.swift
+//  Pee Tracker Watch App
+//
+//  Created by Admin on 10/27/25.
+//
+
+import SwiftUI
+
+@main
+struct Pee_Tracker_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Pee Tracker/Pee_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pee_TrackerApp.swift
+//  Pee Tracker
+//
+//  Created by Admin on 10/27/25.
+//
+
+import SwiftUI
+
+@main
+struct Pee_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
